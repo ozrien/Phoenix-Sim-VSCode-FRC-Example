b0fb2442401d94824e07ca90f4eef3e7ed12fd06
Omar Zrien <ozrien@crosstheroadelectronics.com>
2018-11-06 23:10:11 -0500

    initial commit - created a pristine project and trimmed it.

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+#include "frc/IterativeRobot.h"
+
+class Robot : public frc::IterativeRobot
+{
+  public:
+	void RobotInit()
+	{
+		printf("RobotInit\n");
+	}
+	void TeleopInit()
+	{
+		printf("TeleopInit\n");
+	}
+
+	void TeleopPeriodic()
+	{
+		printf("TeleopPeriodic\n");
+	}
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main()
+{
+	return frc::StartRobot<Robot>();
+}
+#endif
